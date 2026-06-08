ad94cad03e639deb262c67658bacbfc334aadf0c
Brandon Reynolds <breynolds@uttyler.edu>
2026-06-08 15:49:14 -0500

    Delete utils.js

--- utils.js
+++
@@ -1,7 +0,0 @@
-export function safeText(val) {
-  return (val || "").toString().replace(/[<>]/g, "").trim();
-}
-
-export function normalize(str) {
-  return str.toLowerCase().replace(/[_ ]/g, "");
-}
