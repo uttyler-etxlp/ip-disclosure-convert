8a154781935529c6462b2ffa75e871019a74637c
Brandon Reynolds <breynolds@uttyler.edu>
2026-06-30 10:43:13 -0500

    Add FIELD_MAP export for invention details

---
+++ js/fieldmap.js
@@ -0,0 +1,31 @@
+export const FIELD_MAP = {
+  "Descriptive Title of Invention:":
+    "descriptive_title",
+
+  "Corresponding Contributor":
+    "corresponding_contributor",
+
+  "Other Contributors":
+    "other_contributors",
+
+  "Briefly summarize the invention, its use and purpose:":
+    "summary",
+
+  "What particular features of the invention are unusual?":
+    "unusual_features",
+
+  "How does it differ from present technology?":
+    "differ_from_present",
+
+  "What problem(s) does it solve?":
+    "problems_solved",
+
+  "What advantages over current technologies does it posses?":
+    "advantages",
+
+  "Types of intellectual property: - Selected Choice":
+    "ip_types_selected",
+
+  "Types of intellectual property: - Other - Text":
+    "ip_types_other"
+};
